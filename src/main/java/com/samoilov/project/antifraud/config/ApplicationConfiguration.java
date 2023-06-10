334b4dc82dc8d6e6bc523182be029e0dcc78f2d3
vladimir.samoilov <vladimir.samoilov@ecodef.cz>
2023-06-10 13:32:09 +0200

    - added new functionality (new endpoints, new AntifraudInfoMapper, AntifraudInfoChecker) - created repositories to card and ip blacklist - some over code issues fixing

--- src/main/java/com/samoilov/project/antifraud/config/ApplicationConfiguration.java
+++ src/main/java/com/samoilov/project/antifraud/config/ApplicationConfiguration.java
@@ -8,18 +8,53 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 @Configuration
 public class ApplicationConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(List.of(new BasicAuth("basicAuth")))
+                .securityContexts(
+                        List.of(
+                                SecurityContext
+                                        .builder()
+                                        .securityReferences(
+                                                List.of(
+                                                        SecurityReference
+                                                                .builder()
+                                                                .reference("basicAuth")
+                                                                .scopes(new AuthorizationScope[0])
+                                                                .build()
+                                                )
+                                        )
+                                        .build()
+                        )
+                );
+    }
 
     @Bean
     @Primary
     public ObjectMapper objectMapper() {
         return new ObjectMapper()
                 .registerModule(new JavaTimeModule())
-                .setDateFormat(new SimpleDateFormat("MM-yyyy"))
+                .setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"))
                 .setDefaultLeniency(false)
                 .enable(SerializationFeature.INDENT_OUTPUT);
     }
